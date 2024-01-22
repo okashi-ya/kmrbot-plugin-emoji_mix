a36ffd654b27085e44330f864e7713424591cf6e
古守のお菓子屋 <852496245@qq.com>
2024-01-22 20:46:45 +0800

    emoji_mix插件更新读取emoji方法

---
+++ data/__init__.py
@@ -0,0 +1,43 @@
+import json
+import os.path
+from typing import Optional
+from nonebot.log import logger
+
+# emoji_key_data的数据是字典嵌套字典，每一层字典的key都可能是左emojiCode或右emojiCode
+emoji_key_data = {}
+
+
+def init_emoji_data():
+    emoji_key_length = 0
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "metadata.json"), encoding="utf-8") as f:
+        try:
+            for _, data in json.load(f)["data"].items():
+                first_emoji = data["emoji"]
+                emoji_key_data[first_emoji] = {}
+                for combination in data["combinations"]:
+                    l_emoji = combination["leftEmoji"]
+                    r_emoji = combination["rightEmoji"]
+                    # 保证first_emoji和second_emoji是不一样的
+                    second_emoji = r_emoji if l_emoji == first_emoji else l_emoji
+                    emoji_key_data[first_emoji][second_emoji] = {
+                        "url": combination["gStaticUrl"]
+                    }
+                emoji_key_length += len(data["combinations"])
+        except json.JSONDecodeError:
+            logger.error("init_emoji_data json decode fail !")
+            return None
+    logger.info(f"init_emoji_data finish. emoji_key_length = {emoji_key_length}")
+
+
+def get_emoji_url(left_emoji, right_emoji) -> Optional[str]:
+    emoji_data = emoji_key_data.get(left_emoji, {}).get(right_emoji)
+    if emoji_data is None:
+        left_emoji, right_emoji = right_emoji, left_emoji
+
+    emoji_data = emoji_key_data.get(left_emoji, {}).get(right_emoji)
+    if emoji_data is None:
+        return None
+    return emoji_key_data[left_emoji][right_emoji]["url"]
+
+
+init_emoji_data()
--- emoji_mix.py
+++ emoji_mix.py
@@ -6,12 +6,12 @@ from protocol_adapter.protocol_adapter import ProtocolAdapter
 from protocol_adapter.adapter_type import AdapterGroupMessageEvent, AdapterPrivateMessageEvent
 from nonebot import on_regex
 from utils.permission import white_list_handle
-from .emoji_keys_data import emoji_keys_data
+from . import data
 
 emoji_mix = on_regex(
     pattern=r"^.+(\+|\＋).+$",
     priority=5,
-    block=False
+    block=False,
 )
 
 emoji_mix.__doc__ = """emoji_mix"""
@@ -20,48 +20,17 @@ emoji_mix.__help_type__ = None
 emoji_mix.handle()(white_list_handle("emoji_mix"))
 
 
-def split_emoji(event) -> Tuple[bool, list, list]:
+def split_emoji(event) -> Tuple[bool, str, str]:
     if len(event.message) != 1 or event.message[0].type != "text":
-        return False, [], []
+        return False, "", ""
     emoji_data = event.message[0].data.get("text", "").split("+")
-    emoji_keys = ["", ""]
     if len(emoji_data) != 2:
-        return False, [], []
+        return False, "", ""
     for emoji_index in range(len(emoji_data)):
         emoji_data[emoji_index] = emoji_data[emoji_index].replace("\u202a", "").replace("\u202c", "")
         if not is_emoji(emoji_data[emoji_index]):
-            return False, [], []
-        # 一个emoji表情可能有多个字符，组合成规定的样子
-        # emoji_key为不带"u"的emoji内容，用来查询（原生表里就是不带u的，但是发送链接需要带u）
-        emoji_str = ""
-        emoji_key = ""
-        for i in range(len(emoji_data[emoji_index])):
-            if i != 0:
-                emoji_str += "-"
-                emoji_key += "-"
-            h_value = hex(ord(emoji_data[emoji_index][i]))[2:]
-            emoji_str += "u" + h_value
-            emoji_key += h_value
-        emoji_data[emoji_index] = emoji_str
-        emoji_keys[emoji_index] = emoji_key
-    return True, emoji_data, emoji_keys
-
-
-def get_emoji_keys(emoji_data, emoji_keys) -> Union[None, Tuple[str, str, str]]:
-    if emoji_keys_data.get(emoji_keys[0]) is None:
-        if emoji_keys_data.get(emoji_keys[1]) is None:
-            return None
-        else:
-            key_first = emoji_keys[1]   # 第一层的key
-    else:
-        key_first = emoji_keys[0]
-    # 判断date_value是否存在
-    for single_key in emoji_keys_data[key_first]:
-        if emoji_keys[0] == single_key.get("leftEmoji", "") and emoji_keys[1] == single_key.get("rightEmoji", ""):
-            return emoji_data[0], emoji_data[1], single_key.get("date", "")
-        if emoji_keys[1] == single_key.get("leftEmoji", "") and emoji_keys[0] == single_key.get("rightEmoji", ""):
-            return emoji_data[1], emoji_data[0], single_key.get("date", "")
-    return None
+            return False, "", ""
+    return True, emoji_data[0], emoji_data[1]
 
 
 @emoji_mix.handle()
@@ -75,25 +44,17 @@ async def _(
 
     # 仅当符合emoji要求才阻断
     matcher.stop_propagation()
-    result = get_emoji_keys(result[1], result[2])
-    if result is None:
+    url = data.get_emoji_url(result[1], result[2])
+    if url is None:
         msg = ProtocolAdapter.MS.reply(event) + ProtocolAdapter.MS.text("未找到以上两种emoji的mix表情")
-        await emoji_mix.finish(msg)
-        return
-    else:
-        emoji_left = result[0]
-        emoji_right = result[1]
-        emoji_date = result[2]
+        return await emoji_mix.finish(msg)
 
     header = {
         "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"
     }
     async with aiohttp.ClientSession() as session:
         # 遍历所有时间
-        resp = await session.get(
-            f"https://www.gstatic.com/android/keyboard/emojikitchen/{emoji_date}/"
-            f"{emoji_left}/{emoji_left}_{emoji_right}.png",
-            headers=header)
+        resp = await session.get(url, headers=header)
         if resp.status == 200:
             image_data = await resp.read()
             msg = ProtocolAdapter.MS.reply(event) + ProtocolAdapter.MS.image(image_data)
